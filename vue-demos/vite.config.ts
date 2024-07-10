59feb97a7cb9b3059cc4def94af3ddeee3fe2f46
Mapotato-Ma <pf-ma@qq.com>
2024-07-10 23:45:23 +0800

    feat:更换打包配置（输出目录），修改项目根路径

--- vue-demos/vite.config.ts
+++ vue-demos/vite.config.ts
@@ -7,6 +7,8 @@ import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 配置静态资源根路径
+  base: '/mapotato',
   plugins: [vue(), ViteEjsPlugin(), vueJsx()],
   resolve: {
     alias: {
@@ -14,6 +16,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'use-class'
+    outDir: 'mapotato'
   }
 });
